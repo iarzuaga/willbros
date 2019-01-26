38c9f7eabfc2e0b3902d634fc079152ec0197f4b
Ignacio Arzuaga <iarzuaga@sophilabs.com>
2019-01-26 20:06:12 -0300

    Wip throw stun

--- WllBrosProject/Scripts/object.gd
+++ WllBrosProject/Scripts/object.gd
@@ -24,4 +24,14 @@ func _physics_process(delta):
 		if velocity.x != 0:
 			deceleration.x = velocity.x * delta * 2
 		velocity = velocity - deceleration
+		var object_person_collision = self.get_slide_collision(get_slide_count()-1)
+		if object_person_collision:
+			var brother_full = object_person_collision.get_collider_shape().get_parent()
+			if  brother_full.is_in_group("brother"):
+				if brother_full.object_grabbed:
+					print("object attack when full")
+				else:
+					print("object attack while free")
+					
+			
 		self.move_and_slide(velocity)
