ddec0066b1febe8e769e504c0affe41e935122a8
Kateshini <47045892+Kateshini@users.noreply.github.com>
2019-01-27 14:47:30 -0300

    Actualizacion de los menues

--- WllBrosProject/Scripts/Menu.gd
+++ WllBrosProject/Scripts/Menu.gd
@@ -2,22 +2,24 @@ extends Node
 
 export var press1 = false
 export var press2 = false
-var load_time = 3
+var load_time = 1
 
 func _process(delta):
 	
 	if Input.is_action_just_pressed("Interact"):
 		press1 = true
-		$CanvasLayer/obsc1.visible = true
+		$CanvasLayer/red.visible = true
 		
 	elif Input.is_action_just_pressed("Interact_2"):
-		 $CanvasLayer/obsc2.visible = true
+		 $CanvasLayer/blue.visible = true
 		 press2 = true
 		 
 		 
 	elif press1 == true and press2 == true:
+		$CanvasLayer/go.visible = true
 		if load_time <= 0 :
-			get_tree().change_scene("res://Scenes/game_manager.tscn")
+			get_tree().change_scene("res://Scenes/tuto.tscn")
+			
 		else:
 			load_time -= delta
 
