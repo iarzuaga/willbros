40f8a810f7c016cebd058a5b5a2ff5fa1224d37b
Kateshini <47045892+Kateshini@users.noreply.github.com>
2019-01-26 23:41:18 -0300

    Actualiza3

---
+++ WllBrosProject/Scripts/Menu.gd
@@ -0,0 +1,24 @@
+extends Node
+
+export var press1 = false
+export var press2 = false
+var load_time = 3
+
+func _process(delta):
+	
+	if Input.is_action_just_pressed("Interact"):
+		press1 = true
+		$CanvasLayer/obsc1.visible = true
+		
+	elif Input.is_action_just_pressed("Interact_2"):
+		 $CanvasLayer/obsc2.visible = true
+		 press2 = true
+		 
+		 
+	elif press1 == true and press2 == true:
+		if load_time <= 0 :
+			 get_tree().change_scene("res://Scenes/game_manager.tscn")
+		else:
+			load_time -= delta
+
+
