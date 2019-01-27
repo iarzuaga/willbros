8ec2977d9ec3b375f42381e7ad2ea9f5b2ec20bd
Ignacio Arzuaga <iarzuaga@sophilabs.com>
2019-01-27 11:34:06 -0300

    Improved scores

--- WllBrosProject/Scripts/Truck.gd
+++ WllBrosProject/Scripts/Truck.gd
@@ -1,15 +1,15 @@
 extends KinematicBody2D
 
-onready var point_label = get_node("../points")
 var score = 0
 
 func _process(delta):
 	var area = $drop_area
-	var obj_list = area.get_overlapping_bodies()
+	var obj = area.get_overlapping_bodies()
 	
-	for obj in obj_list:
-		if obj.is_in_group("object"):
-			var value = obj.value
-			score += value
-			point_label.set_score(score)
-			obj.get_parent().remove_child(obj)
+	if obj:
+		print(obj[0])
+		if obj[0].is_in_group("object") or true:
+		  var value = obj[0].value
+		  score += value
+		  obj[0].get_parent().remove_child(obj[0])
+		  $points.set_score(score)
